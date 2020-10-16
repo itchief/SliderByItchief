e502bec754e93ae8eda42e32a23dc9c97561807d
itchief <alex.malcev1980@gmail.com>
2020-10-16 23:32:14 +1000

    0.0.2

--- src/slider.js
+++ src/slider.js
@@ -1,113 +1,119 @@
 'use strict';
 
 class SliderByItchief {
+  #positionLeftItem = 0;
+  #wrapperWidth = 0;
+  #itemWidth = 0;
+  #transform = 0;
+  #step = 0;
+  #items = [];
+  #$wrapper = null;
+  #$items = null;
+  #$controls = null;
+
   constructor($elem, config) {
-    this.init($elem, config);
+    this.#init($elem, config);
+    this.#setUpListeners();
   }
-  init($elem, config) {
-    const $wrapper = $elem.querySelector('.slider__wrapper');
-    const $items = $elem.querySelectorAll('.slider__item');
-    const $controls = $elem.querySelectorAll('.slider__control');
-    const $controlLeft = $elem.querySelector('.slider__control_left');
-    const $controlRight = $elem.querySelector('.slider__control_right');
-    const _wrapperWidth = parseFloat(getComputedStyle($wrapper).width); // ширина обёртки
-    const _itemWidth = parseFloat(getComputedStyle($items[0]).width); // ширина одного элемента
-    let _positionLeftItem = 0; // позиция левого активного элемента
-    let _transform = 0; // значение транфсофрмации .slider_wrapper
-    const _step = (_itemWidth / _wrapperWidth) * 100; // величина шага (для трансформации)
-    let _items = []; // массив элементов
-    // наполнение массива _items
-    $items.forEach(function (item, index) {
-      _items.push({ item: item, position: index, transform: 0 });
+
+  #getIndexMin(_items) {
+    let indexItem = 0;
+    _items.forEach(function (item, index) {
+      if (item.position < _items[indexItem].position) {
+        indexItem = index;
+      }
     });
+    return indexItem;
+  }
 
-    var position = {
-      getItemMin: function () {
-        var indexItem = 0;
-        _items.forEach(function (item, index) {
-          if (item.position < _items[indexItem].position) {
-            indexItem = index;
-          }
-        });
-        return indexItem;
-      },
-      getItemMax: function () {
-        var indexItem = 0;
-        _items.forEach(function (item, index) {
-          if (item.position > _items[indexItem].position) {
-            indexItem = index;
-          }
-        });
-        return indexItem;
-      },
-      getMin: function () {
-        return _items[position.getItemMin()].position;
-      },
-      getMax: function () {
-        return _items[position.getItemMax()].position;
-      },
-    };
-
-    var _transformItem = function (direction) {
-      var nextItem;
-      if (direction === 'right') {
-        _positionLeftItem++;
-        if (
-          _positionLeftItem + _wrapperWidth / _itemWidth - 1 >
-          position.getMax()
-        ) {
-          nextItem = position.getItemMin();
-          _items[nextItem].position = position.getMax() + 1;
-          _items[nextItem].transform += _items.length * 100;
-          _items[nextItem].item.style.transform =
-            'translateX(' + _items[nextItem].transform + '%)';
-        }
-        _transform -= _step;
+  #getIndexMax(_items) {
+    let indexItem = 0;
+    _items.forEach(function (item, index) {
+      if (item.position > _items[indexItem].position) {
+        indexItem = index;
       }
-      if (direction === 'left') {
-        _positionLeftItem--;
-        if (_positionLeftItem < position.getMin()) {
-          nextItem = position.getItemMax();
-          _items[nextItem].position = position.getMin() - 1;
-          _items[nextItem].transform -= _items.length * 100;
-          _items[nextItem].item.style.transform =
-            'translateX(' + _items[nextItem].transform + '%)';
-        }
-        _transform += _step;
+    });
+    return indexItem;
+  }
+
+  #move(direction) {
+    let nextItem;
+    if (direction === 'right') {
+      this.#positionLeftItem++;
+      if (
+        this.#positionLeftItem + this.#wrapperWidth / this.#itemWidth - 1 >
+        this.#items[this.#getIndexMax(this.#items)].position
+      ) {
+        nextItem = this.#getIndexMin(this.#items);
+        this.#items[nextItem].position =
+          this.#items[this.#getIndexMax(this.#items)].position + 1;
+        this.#items[nextItem].transform += this.#items.length * 100;
+        this.#items[nextItem].item.style.transform =
+          'translateX(' + this.#items[nextItem].transform + '%)';
       }
-      $wrapper.style.transform = 'translateX(' + _transform + '%)';
-    };
-
-    // обработчик события click для кнопок "назад" и "вперед"
-    var _controlClick = function (e) {
-      if (e.target.classList.contains('slider__control')) {
-        e.preventDefault();
-        var direction = e.target.classList.contains('slider__control_right')
-          ? 'right'
-          : 'left';
-        _transformItem(direction);
+      this.#transform -= this.#step;
+    }
+    if (direction === 'left') {
+      this.#positionLeftItem--;
+      if (
+        this.#positionLeftItem <
+        this.#items[this.#getIndexMin(this.#items)].position
+      ) {
+        nextItem = this.#getIndexMax(this.#items);
+        this.#items[nextItem].position =
+          this.#items[this.#getIndexMin(this.#items)].position - 1;
+        this.#items[nextItem].transform -= this.#items.length * 100;
+        this.#items[nextItem].item.style.transform =
+          'translateX(' + this.#items[nextItem].transform + '%)';
       }
-    };
+      this.#transform += this.#step;
+    }
+    this.#$wrapper.style.transform = 'translateX(' + this.#transform + '%)';
+  }
 
-    var _setUpListeners = function () {
-      // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
-      $controls.forEach(function (item) {
-        item.addEventListener('click', _controlClick);
-      });
-    };
+  // обработчик события click для кнопок "назад" и "вперед"
+  #controlClick(e) {
+    if (e.target.classList.contains('slider__control')) {
+      e.preventDefault();
+      var direction = e.target.classList.contains('slider__control_right')
+        ? 'right'
+        : 'left';
+      this.#move(direction);
+    }
+  }
 
-    // инициализация
-    _setUpListeners();
+  #setUpListeners() {
+    //console.log(that);
+    // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+    const that = this;
+    this.#$controls.forEach(function (item) {
+      item.addEventListener('click', that.#controlClick.bind(that));
+    });
+  }
 
-    return {
-      right: function () {
-        // метод right
-        _transformItem('right');
-      },
-      left: function () {
-        // метод left
-        _transformItem('left');
-      },
-    };
+  #init($elem, config) {
+    this.#$wrapper = $elem.querySelector('.slider__wrapper');
+    this.#$items = $elem.querySelectorAll('.slider__item');
+    this.#$controls = $elem.querySelectorAll('.slider__control');
+    this.#wrapperWidth = parseFloat(getComputedStyle(this.#$wrapper).width); // ширина обёртки
+    this.#itemWidth = parseFloat(getComputedStyle(this.#$items[0]).width); // ширина одного элемента
+    this.#positionLeftItem = 0; // позиция левого активного элемента
+    this.#transform = 0; // значение транфсофрмации .slider_wrapper
+    this.#step = (this.#itemWidth / this.#wrapperWidth) * 100; // величина шага (для трансформации)
+    this.#items = []; // массив элементов
+    // наполнение массива _items
+    const that = this;
+    this.#$items.forEach(function (item, index) {
+      that.#items.push({ item: item, position: index, transform: 0 });
+    });
+    //this.setUpListeners();
+  }
+  right() {
+    // метод right
+    this.#move('right');
+  }
+  left() {
+    // метод left
+    this.#move('left');
   }
 }
